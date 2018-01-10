7d7a689cda76174aafe19d99cfdfe3faa27dc81c
Justin Call <jtcall0121@gmail.com>
2018-01-10 16:49:47 -0500

    Basic CollectionSite class

--- src/CollectionSite.java
+++ src/CollectionSite.java
@@ -1,4 +1,16 @@
+import java.util.Date;
 
+/*
+ *Manages the creation of 'Collection Site' objects
+ */
 public class CollectionSite {
-
+	/*
+	 * Necessary information
+	 */
+	private int siteNumber;
+	private String name, location;
+	private double latitude, longitude;
+	private Date lastAccessed;
+	
+	
 }
